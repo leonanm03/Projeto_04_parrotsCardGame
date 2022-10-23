8890268e38417655b4be3fd96231ed87e6fb971a
leonanm03 <leonanm04@gmail.com>
2022-10-23 13:23:25 -0300

    mantém combinações corretas viradas

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,37 @@
 const images = ["1.gif", "2.gif", "3.gif", "4.gif", "5.gif", "6.gif", "7.gif"]
+let turneds = [];
 
 let num = prompt("Escolha uma quantidade par de cartas entre 4 e 14");
-
-while (num<4 || num> 14 || num%2 != 0){
+while (num < 4 || num > 14 || num % 2 != 0) {
     num = prompt("Eu disse uma quantidade PAR, entre QUATRO(4) e QUARTORZE(14)")
 }
-
-
-
 makeCards(num);
 
-function makeCards(num){
+function makeCards(num) {
     let cardVector = [];
-    for (let i =0; i<num/2; i++){
-        for (let j=0; j<2; j++){
+    for (let i = 0; i < num / 2; i++) {
+        for (let j = 0; j < 2; j++) {
             cardVector.push(images[i]);
         }
     }
 
     cardVector.sort(mixer);
 
-    displayCards(cardVector);
-    
+    implementCards(cardVector);
+
 }
 
-function displayCards(cards){
+// função para auxiliar a embaralhar as cartas
+function mixer() {
+    return Math.random() - 0.5;
+}
+
+function implementCards(cards) {
     const divCards = document.querySelector(".cards");
     console.log(cards);
-    for (let i =0; i<cards.length ; i++){
-            divCards.innerHTML += `
-            <div class="card">
+    for (let i = 0; i < cards.length; i++) {
+        divCards.innerHTML += `
+            <div onclick="turnCard(this)" class="card">
             <div class="front-face face">
                 <img class="parrot" src="./figs/front.png" alt="">
             </div>
@@ -41,8 +43,38 @@ function displayCards(cards){
 
 }
 
+function turnCard(card) {
+    card.classList.toggle("turned");
+    turneds.push(card);
+    console.log(turneds, length);
 
-// função para auxiliar a embaralhar as cartas
-function mixer() { 
-	return Math.random() - 0.5; 
+    if (turneds.length >= 2) {
+        compareCards(turneds[0], turneds[1]);
+
+        turneds = [];
+    }
+
 }
+
+function compareCards(card1, card2) {
+
+
+    if (card1.innerHTML == card2.innerHTML) {
+        alert("acertou");
+        card1.onclick = "";
+        card2.onclick = "";
+        card1.classList.add("correct");
+        card2.classList.add("correct")
+        card1.classList.remove("turned");
+        card2.classList.remove("turned");
+    }
+    else {
+
+        card1.classList.remove("turned");
+        card2.classList.remove("turned");
+
+    }
+
+}
+
+
